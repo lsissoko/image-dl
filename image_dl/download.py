e78e6358a6660a039e48b1d6012f3a01e0f9de00
Lamine Sissoko <lsissoko91@gmail.com>
2015-08-18 00:53:46 -0400

    Fixing merge for real

--- image_dl/download.py
+++ image_dl/download.py
@@ -45,11 +45,8 @@ def download_album(host, url, name, dest=".", delim=" - ", digits=3, number=1):
         hotflick(url, name, dest, delim, digits, number)
     elif host == "myceleb":
         myceleb(url, name, dest, delim, digits, number)
-<<<<<<< HEAD
-=======
     elif host == "mangastream":
         mangastream(url, name, dest, delim, digits, number)
->>>>>>> 85a6964d5af6bf57393151940b2a5177d8250473
     else:
         print "ERROR: Unsupported image host '{}'".format(host)
 
@@ -234,11 +231,8 @@ def upix(url, name, dest, delim, digits, number):
 
 
 def hotflick(url, name, dest, delim, digits, number):
-<<<<<<< HEAD
-=======
     print "Downloading images from [hotflick]...\n"
-
->>>>>>> 85a6964d5af6bf57393151940b2a5177d8250473
+    
     # get all page links if the gallery has more than one page
     div = get_html(url).find('div', {"class": "box-paging"})
     gallery_page_links = [str(tag['href'])
@@ -275,10 +269,6 @@ def hotflick(url, name, dest, delim, digits, number):
 
 
 def myceleb(url, name, dest, delim, digits, number):
-<<<<<<< HEAD
-    new_url = url.replace("myceleb", "hotflick")
-    hotflick(new_url, name, dest, delim, digits, number)
-=======
     print "Downloading images from [myceleb]...\n"
     new_url = url.replace("myceleb", "hotflick")
     hotflick(new_url, name, dest, delim, digits, number)
@@ -301,4 +291,3 @@ def mangastream(url, name, dest, delim, digits, number):
         except:
             print "exception"
             pass
->>>>>>> 85a6964d5af6bf57393151940b2a5177d8250473
